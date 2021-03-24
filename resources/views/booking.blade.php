fce6d4247230a027e65084ef6a4a4d30ffe23440
naylanaja <najanailaa@gmail.com>
2021-03-24 13:34:42 +0700

    booking arsitek progress ref #10

---
+++ resources/views/booking.blade.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+    integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+    <title>Booking: Arsitek</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color:#F0F8FF">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img src="img/Untitled-44.png" style="max-height: 45px;"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" style="background-color:lightgray">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+      <ul class="navbar-nav ml-auto">
+
+        <li class="nav-item ">
+          <a class="nav-link " aria-current="page" href="#">My Order</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">My Buildings</a>
+        </li>
+        <li class="nav-item ">
+          <a class="nav-link" href="#">About Us</a>
+        </li>
+       
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Booking
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Arsitek</a></li>
+            <li><a class="dropdown-item" href="#">Desainer Interior</a></li>
+            <li><a class="dropdown-item" href="#">Tenaga Kerja</a></li>
+          </ul>
+        </li>
+        <li class="nav-item ml-auto">
+          <a class="nav-link" href="#">Testimonials</a>
+        </li>
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+
+    <div class="jumbotron" style="background-image:url('img/5e7ad933cdcaa.jpg')">
+    <h1 style="color:white;text-align:center;font-family:helvetica">Find Your Architect</h1>
+    <p style="color:white;text-align:center;font-family:helvetica">You can start searching by filtering about yout main interest home design,home style.</p>
+    <p style="color:white;text-align:center">and many more so we can offer the best option for you.</p>
+
+    </div>
+
+    <div class="container">
+    <div class="row">
+        <div class="col-sm">
+            <div class="card " style="width: 18rem;background-color:#F0F8FF;border-radius: 10px;margin-left:25px">
+                <div class="card-body">
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" style="background-color:lightgray">
+                    
+                </form>
+                <br>
+                <br>
+                <br>
+                    <p>Filter</p>
+                    <hr style="height:3px">
+                        <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                        <label class="form-check-label" for="flexCheckDefault">
+                            Jakarta
+                        </label>
+                        </div>
+                        <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+                        <label class="form-check-label" for="flexCheckChecked">
+                            Bandung
+                        </label>
+                        
+                        </div>
+                    </div>
+            </div>
+
+        </div>
+        <div class="card mb-3" style="max-width: 540px; margin-right:20px">
+            <div class="row g-0">
+                <div class="col-md-4">
+                <img src="img/5e22a0522d26e.jpg" style="width:180px;height:200px">
+                </div>
+                <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+                </div>
+            </div>
+        </div>
+        
+    
+    </div>
+    </div>
+       
+       
+
+   
+
+   
+     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+</body>
+</html>
