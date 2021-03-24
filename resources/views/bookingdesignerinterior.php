fa9916f182710a9119338835c80c6d6494833ce8
iammcsf <macanseptian@gmail.com>
2021-03-24 13:44:25 +0700

    Booking Desain Interior Ref #7

---
+++ resources/views/bookingdesignerinterior.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <!--Navbar-->
+        <title> Bangun Rumah</title>
+          <meta charset="utf-10">
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="annon">   
+        
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+  <a class="navbar-brand" href="#">
+      <img src="bgnrmh.jpeg" alt="" width="30" height="24" class="d-inline-block align-text-top">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+      <form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+    </form>
+        <a class="nav-link active" aria-current="page" href="#">My Order</a>
+        <a class="nav-link" href="#">My Buildings</a>
+        <a class="nav-link" href="#">About Us</a>
+        <a class="nav-link" href="#">Booking</a>
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Testimonials</a>
+      </div>
+    </div>
+  </div>
+</nav>
+</nav>
