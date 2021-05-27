394289db015603de9470733d982575113647f07b
aulinameidinah <aulinameidinah@gmail.com>
2021-05-27 14:57:03 +0700

    Progress Menawarkan Rancangan ref #16

---
+++ database/migrations/2021_05_26_072223_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('owner');
+            $table->string('projectName');
+            $table->string('ownerPhone');
+            $table->string('projectAddress');
+            $table->string('projectPirce');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Orders');
+    }
+}
